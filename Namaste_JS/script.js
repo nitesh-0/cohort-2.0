1116b59d798c77b5b4c79337506f6799cb0ba76a
nitesh-0 <niteshsah760@gmail.com>
2025-12-31 23:11:35 +0545

    some js stuff

---
+++ Namaste_JS/script.js
@@ -0,0 +1,39 @@
+
+
+function a(){
+    console.log(b)
+}
+
+var b = 9
+a()
+
+
+// getName()
+// console.log(x)
+// console.log(getName)
+
+// var x = 5
+
+// function getName(){
+//     console.log("Namaste Javascript")
+// }
+
+
+
+// var x = 1
+// a()
+// b()
+// console.log(x)
+
+
+// function a(){
+//     var x = 2
+//     console.log(x)
+// }
+
+// function b(){
+//     var x = 3
+//     console.log(x)
+// }
+
+
